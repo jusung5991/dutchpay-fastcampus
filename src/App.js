eab48c0962b2333d6179c7457c28848e3c40fba7
jusung5991 <jusung6583@naver.com>
2024-05-16 23:01:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          귀염둥이 미인즁
         </p>
         <a
           className="App-link"
